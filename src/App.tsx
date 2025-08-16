6110ce47127b9c87206cec4aef93e56992215660
nguyennguyen21 <khainguyentranho@gmain.com>
2025-08-16 12:32:21 +0700

    xu ly xung dot git

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-<<<<<<< HEAD
 
-=======
+
+
 import AppRoutes from './routes/Index'
->>>>>>> 82151f4e8b43b1bf1323dc58d775a0d1d8792927
+
 import './App.css'
 import Login from './modules/Athur/component/LoginForm';
 
 
 function App() {
   return (
-<<<<<<< HEAD
+
    <>
    <Login/>
    </>
